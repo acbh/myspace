a32479d0c4c3f885bbd49f5a00c33d49b530ab1b
H B <3343806536@qq.com>
2022-10-15 10:55:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavBar />
+  <router-view/>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';//当界面缩小时能将折叠的菜单打开
+import NavBar from './components/NavBar';
+
+
+export default{
+  name:"App",
+  components:{
+    NavBar,
+
+}
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <contentBase>
+    首页
+  </contentBase>
+</template>
+
+<script>
+import ContentBase from'../components/ContentBase';
+
+export default {
+  name: 'HomeView',
+  components: {
+    ContentBase,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
